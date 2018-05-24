af40ad2b7883f0a11896f8859cdee53c040c8bf3
EAWuchner <39601601+EAWuchner@users.noreply.github.com>
2018-05-24 13:02:42 -0700

    Add files via upload

---
+++ Boggle/Turn.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class Turn here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.time.LocalTime;
+public class Turn
+{
+    public Turn()
+    {
+        /*input: Boggle
+        input: Player[]
+        output: Player[].score
+        for int i = 0; i < number of Players; i++
+            LocalTime endTurn = LocalTime.minusMinutes(3);
+            while LocalTime.now != endturn
+                let players add words in an array list
+        check to see if any words are duplicates between lists, delete them from all lists
+        check to see if any words are < 3 letters, delete them
+        score lists of words
+        PlayerScores += score of words
+        */
+    }
+}
