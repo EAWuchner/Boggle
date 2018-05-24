d354fc31254f6740dd03cac2c284b04b4d95fde6
caitlinhruby <39601732+caitlinhruby@users.noreply.github.com>
2018-05-24 13:07:38 -0700

    Add files via upload

---
+++ Boggle/BOGGLE/Boggle.java
@@ -0,0 +1,41 @@
+import java.util.Random;
+import java.util.ArrayList;
+
+public class Boggle
+{
+    char[][] board = new char[5][5];
+    public Boggle()
+    {
+        ArrayList<Character> letters = new ArrayList<>();
+        String alphabet = "aabcddeefghhiijkllmnnoopqrrssttuuvwxyz";
+        Random num = new Random();
+        for (int f = 0; f < alphabet.length(); f++)
+        {
+            letters.add(alphabet.charAt(f));
+        }
+        
+        
+        for (int i = 0; i < board.length; i++)
+        {
+            for (int j = 0; j < board.length; j++)
+            {
+                int letter = num.nextInt(letters.size());
+                board[i][j] = alphabet.charAt(letter);
+                letters.remove(letter);
+            }
+        }
+    }
+    
+    public void toStringMeth()
+    {
+        String boards="";
+        for (int i = 0; i < board.length; i++)
+        {
+            for (int j = 0; j < board.length; j++)
+            {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
