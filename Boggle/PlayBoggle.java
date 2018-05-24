15e28490ed4ac3e28d2d8e5b04e2cfe1f298a708
EAWuchner <39601601+EAWuchner@users.noreply.github.com>
2018-05-24 13:16:10 -0700

    Add files via upload
    Uses Caitlin's board

---
+++ Boggle/PlayBoggle.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of class Turn here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.time.LocalTime;
+public class PlayBoggle
+{
+    /**
+     * A round of Boggle is played
+     * @param   players     an array filled with Boggle players
+     */
+    public PlayBoggle()
+    {
+        Boggle board = new Boggle();
+        /*input:
+        input: Player[]
+        output: Player[].score
+        for int i = 0; i < number of Players; i++
+            LocalTime endTurn = LocalTime.minusMinutes(3);
+            while LocalTime.now != endturn
+                let players add words in an array list
+        check to see if any words are duplicates between lists, delete them from all lists
+        check to see if any words are < 3 letters, delete them
+        make scores
+        PlayerScore += scores
+        return player[]
+        */
+    }
+}
